3864a070d8a33477dd050c6ed25e48ae8b7cf99d
Kerui Tan <ktan@iastate.edu>
2018-11-05 07:23:33 -0600

    dll not building

--- Cheese/Cheese.h
+++ Cheese/Cheese.h
@@ -1,5 +1,6 @@
 // Cheese.h - Contains declarations of mouse hook functions
 #pragma once
+#include <tchar.h>
 
 #ifdef CHEESE_EXPORTS
 #define CHEESE_API __declspec(dllexport)
@@ -7,9 +8,10 @@
 #define CHEESE_API __declspec(dllimport)
 #endif
 
+#define UWM_MOUSEMOVE_MSG _T("UWM_MOUSEMOVE-016F0F50-40F8-4276-93B5-151FB0B24E8A")
+
 // Sets mouse hook
-extern "C" CHEESE_API void setHook(
-	const unsigned long long a, const unsigned long long b);
+extern "C" CHEESE_API BOOL setHook(HWND hWnd);
 
 // Unhooks mouse hook
-extern "C" CHEESE_API bool unhook();
+extern "C" CHEESE_API BOOL unHook(HWND hWnd);
