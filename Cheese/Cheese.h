1e0c1d6c20550b73ac1e29c0fe5e3e1e4ccf99e9
Kerui Tan <ktan@iastate.edu>
2018-11-07 00:00:45 -0600

    deleted user message

--- Cheese/Cheese.h
+++ Cheese/Cheese.h
@@ -8,8 +8,6 @@
 #define CHEESE_API __declspec(dllimport)
 #endif
 
-#define UWM_MOUSEMOVE_MSG _T("UWM_MOUSEMOVE-016F0F50-40F8-4276-93B5-151FB0B24E8A")
-
 // Sets mouse hook
 extern "C" CHEESE_API BOOL setHook(HWND hWnd);
 
